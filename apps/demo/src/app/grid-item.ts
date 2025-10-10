53356e85117903ddbe11a4a2361022e4fdd65962
Daniel Beck <daniel.beck@hessian.ai>
2025-10-10 11:46:09 +0200

    Refactor template structure to fix alignment error when text wraps.

--- apps/demo/src/app/grid-item.ts
+++ apps/demo/src/app/grid-item.ts
@@ -1,4 +1,3 @@
-
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
@@ -12,61 +11,56 @@ import {
 import { RandomPhotoComponent } from './random-photo.component';
 import { StarIconComponent } from './star-icon.component';
 import { ScrollGridItem } from './types';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'demo-grid-item',
   standalone: true,
-  imports: [RandomPhotoComponent, StarIconComponent],
+  imports: [RandomPhotoComponent, StarIconComponent, NgClass],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [
-    `
-      :host {
-        @apply bg-white rounded-lg max-w-full max-h-full flex flex-col shadow-md overflow-hidden transform transition hover:scale-105 min-w-8;
-        &.selected {
-          @apply ring-4 ring-blue-500;
-        }
-        &.listItem {
-          @apply min-w-64;
-        }
-      }
-    `,
-  ],
   template: `
-    <demo-random-photo
-      class="w-full h-[30%]flex-shrink-0 overflow-hidden"
-      [seed]="item().id"
-    ></demo-random-photo>
-    <div class="p-4 flex flex-col flex-1 w-full">
-      <div class="w-full flex-shrink-0 flex flex-row overflow-hidden ">
-        <h2
-          [id]="headingId()"
-          class="text-xl font-bold mb-2 text-gray-800 flex-1 truncate flex w-full gap-2 items-center"
-        >
-          Card {{ item().index }}
-        </h2>
+    <div
+      class="bg-white rounded-lg max-w-full max-h-full h-full flex flex-col shadow-md overflow-hidden transform transition hover:scale-105 min-w-8 group"
+      [ngClass]="{
+        'ring-4  ring-blue-500 !bg-blue-50': this.isSelected()
+      }"
+    >
+      <demo-random-photo
+        class="w-full h-[30%]flex-shrink-0 overflow-hidden"
+        [seed]="item().id"
+      ></demo-random-photo>
+      <div class="p-4 flex flex-col flex-1 w-full">
+        <div class="w-full flex-shrink-0 flex flex-row overflow-hidden ">
+          <h2
+            [id]="headingId()"
+            class="text-xl font-bold mb-2 text-gray-800 flex-1 truncate flex w-full gap-2 items-center"
+          >
+            Card {{ item().index }}
+          </h2>
 
-        <div
-          class="flex-shrink-0"
-          [id]="starId()"
-          [attr.data-starred]="item().isFavored"
-        >
-          @if (item().isFavored; as isFavored) {
-          <demo-star-icon [isStarred]="isFavored"></demo-star-icon>
-          }
+          <div
+            class="flex-shrink-0"
+            [id]="starId()"
+            [attr.data-starred]="item().isFavored"
+          >
+            @if (item().isFavored; as isFavored) {
+            <demo-star-icon [isStarred]="isFavored"></demo-star-icon>
+            }
+          </div>
         </div>
-      </div>
 
-      <p class="text-gray-600 mb-4 flex-1 line-clamp-2">
-        This item is in row {{ row() }} and in column {{ column() }}
-      </p>
-      <button
-        class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition flex-shrink-0 truncate cursor-pointer"
-        (click)="selectItem()"
-        (pointerup)="selectItem()"
-        [id]="buttonId()"
-      >
-        Learn More
-      </button>
+        <p class="text-gray-600 mb-4 flex-1 line-clamp-2 truncate min-w-0 text-sm">
+          This item is in row {{ row() }} and in column {{ column() }}
+        </p>
+        <button
+          class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition flex-shrink-0 truncate cursor-pointer"
+          (click)="selectItem()"
+          (pointerup)="selectItem()"
+          [id]="buttonId()"
+        >
+          Learn More
+        </button>
+      </div>
     </div>
   `,
 })
@@ -92,9 +86,6 @@ export class GridItemComponent {
   constructor(cdr: ChangeDetectorRef) {
     effect(() => {
       const classes: string[] = [];
-      if (this.isSelected()) {
-        classes.push('selected');
-      }
       if (this.type() === 'list') {
         classes.push('listItem');
       }
